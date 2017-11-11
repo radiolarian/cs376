8a8c6ce6cf258cd1657b61b8f77fa632e85576b6
Evan Strasnick <estrasnick@gmail.com>
2017-11-11 14:34:52 -0800

    Add new gradle imports that will allow us to use database library

---
+++ mobile/src/main/java/com/example/noon/cs376/MainDatabase.java
@@ -0,0 +1,11 @@
+package com.example.noon.cs376;
+
+/**
+ * Created by estra on 11/11/2017.
+ */
+
+import android.arch.persistence.room.RoomDatabase;
+
+public abstract class MainDatabase extends RoomDatabase {
+}
+
